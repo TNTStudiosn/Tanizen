12e696056982f76b91b630821d3e6b84878f2bef
TNTStudios <tntstudiosn@gmail.com>
2025-04-03 03:36:49 -0600

    inicio

---
+++ src/client/java/com/TNTStudios/tanizen/client/TanizenClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.tanizen.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TanizenClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/tanizen/Tanizen.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.tanizen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tanizen implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
